d5d0c851da617be4990c47e46f107ce7c1be261f
Gopal Sasmal <74035853+gopal24-colab@users.noreply.github.com>
2023-01-04 16:49:13 +0000

    #

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Welcome to codespaces");
+
+
+import app from "./app";
+const {PORT} = process.env;
+
+app.listen(PORT,(err) => {
+  if (err) console.log(`Error due to ${err}`);
+  console.log(`Server started`);
+});
